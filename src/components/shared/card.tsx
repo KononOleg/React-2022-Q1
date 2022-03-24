5a424e675f4c13669bd259c6cd21870b5b6ba85e
KononOleg <konon.oleg97@gmail.com>
2022-03-24 16:35:31 +0300

    feat: add card

---
+++ src/components/shared/card.tsx
@@ -0,0 +1,56 @@
+import "./card.css";
+
+interface IProps {
+  logo: string;
+  title: string;
+  year: string;
+  age: string;
+  season: string;
+  genre: string;
+  description: string;
+  cast: string;
+  creators: string;
+  background: string;
+}
+
+export const Card = (props: IProps) => {
+  return (
+    <div className="card">
+      <div className="card__wrapper">
+        <div className="card__details">
+          <div className="card-details__wrapper">
+            <img
+              className="card-details__logo"
+              src={props.logo}
+              alt={props.title}
+            />
+            <h2 className="card-details__title">{props.title}</h2>
+            <div className="card-details__info">
+              <span className="card-info__text">{props.year}</span>
+              <span className="card-info__text">{props.age}</span>
+              <span className="card-info__text">{props.season}</span>
+              <span className="card-info__text">{props.genre}</span>
+            </div>
+            <p className="card-details__description">{props.description}</p>
+            <div className="card-details__talent">
+              <div className="card-talent__wrapper">
+                <span className="card-talent__label">В ролях:</span>
+                <p className="card-talent__info">{props.cast}</p>
+              </div>
+              <div className="card-talent__wrapper">
+                <span className="card-talent__label">Создатели:</span>
+                <p className="card-talent__info">{props.creators}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-image__container">
+          <div
+            className="card__image"
+            style={{ backgroundImage: `url(${props.background})` }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
