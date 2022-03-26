cebdf6093be6fa851fa6df7f21fabd33527ea635
KononOleg <konon.oleg97@gmail.com>
2022-03-26 19:26:32 +0300

    feat: add main page

---
+++ src/components/pages/main-page.tsx
@@ -0,0 +1,14 @@
+import { SearchBar } from "../shared/search-bar/search-bar";
+import { Slider } from "../shared/slider/slider";
+import "./main-page.css";
+
+export const MainPage = () => {
+  return (
+    <div className="main">
+      <div className="search-bar__contain">
+        <SearchBar />
+      </div>
+      <Slider />
+    </div>
+  );
+};
