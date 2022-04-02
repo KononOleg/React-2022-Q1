2abc5797627d6fd524eb3af3160a0bd6d13400e0
KononOleg <konon.oleg97@gmail.com>
2022-04-02 17:15:14 +0300

    feat: add form

---
+++ src/components/pages/form-page/components/form/form.tsx
@@ -0,0 +1,5 @@
+import './form.css';
+
+export const Form = () => {
+  return <div className="form__wrapper"></div>;
+};
--- src/components/pages/form-page/form-page.tsx
+++ src/components/pages/form-page/form-page.tsx
@@ -1,5 +1,10 @@
+import { Form } from './components/form/form';
 import './form-page.css';
 
 export const FormPage = () => {
-  return <div className="form-page__wrapper"></div>;
+  return (
+    <div className="form-page__wrapper">
+      <Form />
+    </div>
+  );
 };
