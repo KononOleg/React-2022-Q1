abfdecf8a4f35cfebf819c6e70cf450904dc232f
KononOleg <konon.oleg97@gmail.com>
2022-04-03 20:46:08 +0300

    feat: add validation step 1

--- src/components/pages/form-page/components/form/components/input/text/text.tsx
+++ src/components/pages/form-page/components/form/components/input/text/text.tsx
@@ -1,16 +1,11 @@
+import React from 'react';
 import { InputWrapper } from '../wrapper/wrapper';
 import './text.css';
 
-interface IProps {
-  label: string;
-  errorMessage: string;
-  isError: boolean;
-}
-
-export const InputText: React.FC<IProps> = ({ label, errorMessage, isError }) => {
+export const InputText: React.FC<any> = ({ label, errorMessage, isError, inputRef, OnChange }) => {
   return (
     <InputWrapper {...{ label, errorMessage, isError }}>
-      <input type="text" className="input-text" />
+      <input type="text" className="input-text" ref={inputRef} onChange={OnChange} />
     </InputWrapper>
   );
 };
--- src/components/pages/form-page/components/form/form.tsx
+++ src/components/pages/form-page/components/form/form.tsx
@@ -1,5 +1,47 @@
+import React from 'react';
+import { InputText } from './components/input/text/text';
 import './form.css';
 
-export const Form = () => {
-  return <div className="form__wrapper"></div>;
-};
+export class Form extends React.Component<any, any> {
+  input: any;
+  constructor(props: any) {
+    super(props);
+    this.state = { isValid: { isNameValid: false }, Name: { isError: false, Message: '' } };
+    this.input = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.set = this.set.bind(this);
+    this.vali = this.vali.bind(this);
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+    console.log('dsadas');
+    this.vali(this.state);
+  }
+
+  vali(state: any) {
+    if (this.input) {
+      this.setState({ Name: { isError: true, Message: 'dsadas' } });
+    }
+  }
+  set() {
+    const r = 'Name';
+    this.setState(`{ ${r}: { isError: false } }`);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form__wrapper">
+        <InputText
+          inputRef={this.input}
+          errorMessage={this.state.Name.Message}
+          isError={this.state.Name.isError}
+          OnChange={() => {
+            this.setState({ Name: { isError: false } });
+          }}
+        />
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }
+}
