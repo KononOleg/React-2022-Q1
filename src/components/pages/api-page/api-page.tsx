9ac251e876f8d9b183f3367dc7667164a7172c32
KononOleg <konon.oleg97@gmail.com>
2022-04-09 17:24:31 +0300

    feat: add search bar

--- src/components/pages/api-page/api-page.tsx
+++ src/components/pages/api-page/api-page.tsx
@@ -1,5 +1,24 @@
+import React from 'react';
 import './api-page.css';
+import { SearchBar } from './components/search-bar/search-bar';
 
-export const ApiPage = () => {
-  return <div className="api-page__wrapper"></div>;
-};
+export class ApiPage extends React.Component<unknown, unknown> {
+  constructor(props: unknown) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(input: string) {}
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="api-page">
+        <div className="api-page__wrapper">
+          <SearchBar handleSubmit={this.handleSubmit} />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/pages/api-page/components/search-bar/search-bar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './search-bar.css';
+
+interface IProps {
+  handleSubmit: (input: string) => void;
+}
+
+export class SearchBar extends React.Component<IProps, unknown> {
+  input: React.RefObject<HTMLInputElement>;
+  constructor(props: IProps) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
+
+  handleSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    this.props.handleSubmit(this.input.current?.value as string);
+  }
+
+  render() {
+    return (
+      <div className="search-bar-api__wrapper">
+        <h2 className="search-bar-api__title">Добро пожаловать.</h2>
+        <h3 className="search-bar-api__subtitle">
+          Миллионы фильмов, сериалов и людей. Исследуйте сейчас.
+        </h3>
+        <form className="search-bar-api" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className="search-bar-api__input"
+            placeholder="Найти фильм, сериал, персону......"
+            ref={this.input}
+          />
+          <input type="submit" className="search-bar-api__submit" value="Search" />
+        </form>
+      </div>
+    );
+  }
+}
