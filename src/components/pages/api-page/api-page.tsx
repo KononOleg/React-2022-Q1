4741182ffcd56cca10f8856ca2abc1bac3292a5d
KononOleg <konon.oleg97@gmail.com>
2022-04-10 16:18:42 +0300

    feat: add card api

--- src/components/pages/api-page/api-page.tsx
+++ src/components/pages/api-page/api-page.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import './api-page.css';
+import { CardApi } from './components/card/card';
 import { SearchBar } from './components/search-bar/search-bar';
+import { getPopularMovie } from './shared/api';
 
-export class ApiPage extends React.Component<unknown, unknown> {
+export interface ICard {
+  id: string;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
+
+interface IState {
+  cards: ICard[];
+}
+
+export class ApiPage extends React.Component<unknown, IState> {
   constructor(props: unknown) {
     super(props);
+    this.state = { cards: [] };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(input: string) {}
 
-  componentDidMount() {}
+  async componentDidMount() {
+    const response = await getPopularMovie();
+    this.setState({ cards: response.results });
+  }
 
   render() {
     return (
       <div className="api-page">
         <div className="api-page__wrapper">
           <SearchBar handleSubmit={this.handleSubmit} />
+          <div className="api-cards__container">
+            {this.state.cards.map((card: ICard) => (
+              <CardApi {...card} key={card.id} />
+            ))}
+          </div>
         </div>
       </div>
     );
---
+++ src/components/pages/api-page/components/card/card.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ICard } from '../../api-page';
+import './card.css';
+
+export class CardApi extends React.Component<ICard, unknown> {
+  render() {
+    const { id, title, poster_path, release_date } = this.props;
+    return (
+      <div className="card-api__wrapper">
+        <img
+          className="card-api__img"
+          src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${poster_path}`}
+        />
+        <div className="card-api__content">
+          <h2 className="card-api__title">{title}</h2>
+          <p className="card-api__date">{release_date}</p>
+        </div>
+      </div>
+    );
+  }
+}
